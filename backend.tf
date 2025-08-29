6e443d0c67535cb8efefb7439a9a86408902a884
vaibhavpatil7111 <vaihavborse0019@gmail.com>
2025-08-29 14:57:37 +0530

    region change

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
         key = "cicd/terraform.tfstate"
         region = "us-east-1"
         encrypt = true
-        dynamodb_table = "terraform-locks"
+        use_lockfile = true
     }
 }
