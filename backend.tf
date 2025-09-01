d83a749e070ccf4e4c5711babc5f776bbc913da6
vaibhavpatil7111 <vaihavborse0019@gmail.com>
2025-09-01 19:00:42 +0530

    changes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "my-terraform-state-bucket"
+        bucket = "my-terraform-state-bucket-211125508265"
         key = "cicd/terraform.tfstate"
         region = "us-east-1"
         encrypt = true
--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -25,6 +25,40 @@ resource "aws_security_group" "main" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+# For the Jenkins
+
+resource "aws_security_group" "jenkins_sg" {
+  name = "jenkins-sg"
+  vpc_id = var.vpc_id
+
+    ingress {
+    description = "Jenkins"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "sonar-qube"
+    from_port = 9000
+    to_port = 9000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port = 0
@@ -32,7 +66,25 @@ resource "aws_security_group" "main" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    Name = "${var.project_name}-sg"
+}
+
+
+resource "aws_security_group" "app_sg" {
+  name = "app-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+  
